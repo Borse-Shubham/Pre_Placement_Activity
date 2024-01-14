f4c6ee310f4e3f4184930bbb31581045391ecc19
Borse-Shubham <shubhamborse0911@gmail.com>
2024-01-14 13:46:36 +0530

    GUI Calculator Using AWT

---
+++ Java/Calulator(GUI)/MarvellousCalculater.java
@@ -0,0 +1,30 @@
+import java.swing.*;
+import java.awt.event.*;
+
+class Calculator extends WindowAdapter implements ActionListener
+{
+    public JFrame mainframe;
+
+    public Calculator(int width, int height, String title)
+    {
+        mainframe = new JFrame(title);
+        
+        mainframe.setSize(width,height);
+        mainframe.setVisible(true);
+    }
+
+    public void windowClosing(WindowEvent obj)
+    {
+        System.exit(0);
+    }
+
+    public void actionPerformed(ActionEvent obj){}
+}
+
+class MarvellousCalculater
+{
+    public static void main(String arg[])
+    {
+        Calculator cobj = new Calculator(400,400,"Marvellous Calculator");
+    }
+}
